82bcfc4d6eb781b7efc2329f35661ba8a3980a5d
attorney2005 <72994919+attorney2005@users.noreply.github.com>
2022-03-02 10:59:49 +0300

    Сделал форму отправки

--- project/src/components/room-screen/room-screen.tsx
+++ project/src/components/room-screen/room-screen.tsx
@@ -1,20 +1,21 @@
-import { Link } from 'react-router-dom';
-import { useLocation } from 'react-router-dom';
-import { offers } from '../../mocks/offers';
-import { Review } from '../../types/review';
+import {Link} from 'react-router-dom';
+import {useLocation} from 'react-router-dom';
+import {offers} from '../../mocks/offers';
+import {Review} from '../../types/review';
+import SubmitCommentForm from '../submit-form/submit-form';
 
 type RoomScreenProps = {
   reviews: Review[]
 };
 
-function RoomScreen({reviews}: RoomScreenProps){
+function RoomScreen({reviews}: RoomScreenProps) {
   const location = useLocation();
   const pathElements = location.pathname.split('/');
-  const offerId =  parseInt(pathElements[pathElements.length - 1], 10);
+  const offerId = parseInt(pathElements[pathElements.length - 1], 10);
   const [offer] = offers.filter((currentOffer) =>
     currentOffer.id === offerId);
 
-  const nearOffers = offers.slice(0, 3);
+  // const nearOffers = offers.slice(0, 3);
 
   return (
     <>
@@ -99,7 +100,7 @@ function RoomScreen({reviews}: RoomScreenProps){
                     <div className="property__avatar-wrapper property__avatar-wrapper--pro user__avatar-wrapper">
                       <img className="property__avatar user__avatar"
                            src={offer.owner.avatarImage}
-                        width="74" height="74" alt="Host avatar"
+                           width="74" height="74" alt="Host avatar"
                       />
                     </div>
                     <span className="property__user-name">
@@ -120,94 +121,8 @@ function RoomScreen({reviews}: RoomScreenProps){
                   </div>
                 </div>
                 <section className="property__reviews reviews">
-                  <h2 className="reviews__title">Reviews &middot; <span className="reviews__amount">1</span></h2>
-                  <ul className="reviews__list">
-                    <li className="reviews__item">
-                      <div className="reviews__user user">
-                        <div className="reviews__avatar-wrapper user__avatar-wrapper">
-                          <img className="reviews__avatar user__avatar" src="../project/public/img/avatar-max.jpg"
-                            width="54" height="54" alt="Reviews avatar"
-                          />
-                        </div>
-                        <span className="reviews__user-name">
-                        Max
-                        </span>
-                      </div>
-                      <div className="reviews__info">
-                        <div className="reviews__rating rating">
-                          <div className="reviews__stars rating__stars">
-                            <span style={{width: '80%'}}></span>
-                            <span className="visually-hidden">Rating</span>
-                          </div>
-                        </div>
-                        <p className="reviews__text">
-                          A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.
-                          The building is green and from 18th century.
-                        </p>
-                        <time className="reviews__time" dateTime="2019-04-24">April 2019</time>
-                      </div>
-                    </li>
-                  </ul>
-                  <form className="reviews__form form" action="#" method="post">
-                    <label className="reviews__label form__label" htmlFor="review">Your review</label>
-                    <div className="reviews__rating-form form__rating">
-                      <input className="form__rating-input visually-hidden" name="rating" value="5" id="5-stars"
-                        type="radio"
-                      />
-                      <label htmlFor="5-stars" className="reviews__rating-label form__rating-label" title="perfect">
-                        <svg className="form__star-image" width="37" height="33">
-                          <use xlinkHref="#icon-star"></use>
-                        </svg>
-                      </label>
-
-                      <input className="form__rating-input visually-hidden" name="rating" value="4" id="4-stars"
-                        type="radio"
-                      />
-                      <label htmlFor="4-stars" className="reviews__rating-label form__rating-label" title="good">
-                        <svg className="form__star-image" width="37" height="33">
-                          <use xlinkHref="#icon-star"></use>
-                        </svg>
-                      </label>
-
-                      <input className="form__rating-input visually-hidden" name="rating" value="3" id="3-stars"
-                        type="radio"
-                      />
-                      <label htmlFor="3-stars" className="reviews__rating-label form__rating-label" title="not bad">
-                        <svg className="form__star-image" width="37" height="33">
-                          <use xlinkHref="#icon-star"></use>
-                        </svg>
-                      </label>
-
-                      <input className="form__rating-input visually-hidden" name="rating" value="2" id="2-stars"
-                        type="radio"
-                      />
-                      <label htmlFor="2-stars" className="reviews__rating-label form__rating-label" title="badly">
-                        <svg className="form__star-image" width="37" height="33">
-                          <use xlinkHref="#icon-star"></use>
-                        </svg>
-                      </label>
-
-                      <input className="form__rating-input visually-hidden" name="rating" value="1" id="1-star"
-                        type="radio"
-                      />
-                      <label htmlFor="1-star" className="reviews__rating-label form__rating-label" title="terribly">
-                        <svg className="form__star-image" width="37" height="33">
-                          <use xlinkHref="#icon-star"></use>
-                        </svg>
-                      </label>
-                    </div>
-                    <textarea className="reviews__textarea form__textarea" id="review" name="review"
-                      placeholder="Tell how was your stay, what you like and what can be improved"
-                    >
-                    </textarea>
-                    <div className="reviews__button-wrapper">
-                      <p className="reviews__help">
-                        To submit review please make sure to set <span className="reviews__star">rating</span> and describe
-                        your stay with at least <b className="reviews__text-amount">50 characters</b>.
-                      </p>
-                      <button className="reviews__submit form__submit button" type="submit" disabled>Submit</button>
-                    </div>
-                  </form>
+                  {/*<Reviews reviews={reviews}/>*/}
+                  <SubmitCommentForm/>
                 </section>
               </div>
             </div>
@@ -219,11 +134,15 @@ function RoomScreen({reviews}: RoomScreenProps){
               <div className="near-places__list places__list">
                 <article className="near-places__card place-card">
                   <div className="near-places__image-wrapper place-card__image-wrapper">
-                    <a href="#">
-                      <img className="place-card__image" src="../project/public/img/room.jpg" width="260" height="200"
-                        alt="Place image"
+                    <Link to={`/offer/${offer.id}`}>
+                      <img
+                        className="place-card__image"
+                        src={offer.previewImage}
+                        width="260"
+                        height="200"
+                        alt="Place im"
                       />
-                    </a>
+                    </Link>
                   </div>
                   <div className="place-card__info">
                     <div className="place-card__price-wrapper">
@@ -232,7 +151,7 @@ function RoomScreen({reviews}: RoomScreenProps){
                         <span className="place-card__price-text">&#47;&nbsp;night</span>
                       </div>
                       <button className="place-card__bookmark-button place-card__bookmark-button--active button"
-                        type="button"
+                              type="button"
                       >
                         <svg className="place-card__bookmark-icon" width="18" height="19">
                           <use xlinkHref="#icon-bookmark"></use>
@@ -255,11 +174,15 @@ function RoomScreen({reviews}: RoomScreenProps){
 
                 <article className="near-places__card place-card">
                   <div className="near-places__image-wrapper place-card__image-wrapper">
-                    <a href="#">
-                      <img className="place-card__image" src="../project/public/img/apartment-02.jpg" width="260"
-                        height="200" alt="Place image"
+                    <Link to={`/offer/${offer.id}`}>
+                      <img
+                        className="place-card__image"
+                        src={offer.previewImage}
+                        width="260"
+                        height="200"
+                        alt="Place im"
                       />
-                    </a>
+                    </Link>
                   </div>
                   <div className="place-card__info">
                     <div className="place-card__price-wrapper">
@@ -292,11 +215,15 @@ function RoomScreen({reviews}: RoomScreenProps){
                     <span>Premium</span>
                   </div>
                   <div className="near-places__image-wrapper place-card__image-wrapper">
-                    <a href="#">
-                      <img className="place-card__image" src="../project/public/img/apartment-03.jpg" width="260"
-                        height="200" alt="Place image"
+                    <Link to={`/offer/${offer.id}`}>
+                      <img
+                        className="place-card__image"
+                        src={offer.previewImage}
+                        width="260"
+                        height="200"
+                        alt="Place im"
                       />
-                    </a>
+                    </Link>
                   </div>
                   <div className="place-card__info">
                     <div className="place-card__price-wrapper">
