3ae9d52fef656708a0d3b59a260dc38f54ed5604
attorney2005 <72994919+attorney2005@users.noreply.github.com>
2022-03-11 19:04:01 +0300

    Начал делать карту

---
+++ project/src/components/map/map.tsx
@@ -0,0 +1,61 @@
+import {useEffect, useRef} from 'react';
+import leaflet, { Marker } from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+import useMap from '../../hooks/use-map';
+import {City, Offer, Offers} from '../../types/offer';
+import {URL_MARKER_DEFAULT, URL_MARKER_CURRENT} from '../../constant';
+
+type MapProps = {
+  city: City;
+  offers: Offers;
+  activeOffer: Offer | null;
+}
+
+function Map({city, offers, activeOffer}: MapProps) {
+  const mapRef = useRef(null);
+  const map = useMap(mapRef, city);
+
+  const defaultCustomIcon = leaflet.icon({
+    iconUrl: URL_MARKER_DEFAULT,
+    iconSize: [40, 40],
+    iconAnchor: [20, 40],
+  });
+
+  const currentCustomIcon = leaflet.icon({
+    iconUrl: URL_MARKER_CURRENT,
+    iconSize: [40, 40],
+    iconAnchor: [20, 40],
+  });
+  useEffect(() => {
+    const markers: leaflet.Marker[] = [];
+
+    if (map) {
+      offers.forEach((offer) => {
+        const marker = leaflet
+          .marker({
+            lat: offer.location.latitude,
+            lng: offer.location.longitude,
+          }, {
+            icon: (activeOffer && offer.location.latitude === activeOffer.location.latitude && offer.location.longitude === activeOffer.location.longitude)
+              ? currentCustomIcon
+              : defaultCustomIcon,
+          });
+        marker.addTo(map);
+        markers.push(marker);
+      });
+      return () => markers.forEach((marker) => {
+        marker.removeFrom(map);
+      });
+    }
+  }, [map, offers]);
+
+  return (
+    <div
+      style={{height: '100%'}}
+      ref={mapRef}
+    >
+    </div>
+  );
+}
+
+export default Map;
--- project/src/mocks/offers.ts
+++ project/src/mocks/offers.ts
@@ -137,14 +137,14 @@ export const offers: Offer[] = [
     city: {
       name: 'Amsterdam',
       location: {
-        latitude: 2,
-        longitude: 2,
+        latitude: 52.3909553943508,
+        longitude: 4.85309666406198,
         zoom: 10,
       },
     },
     location: {
-      latitude: 2,
-      longitude: 2,
+      latitude: 52.369553943508,
+      longitude: 4.85309666406198,
       zoom: 10,
     },
   },
