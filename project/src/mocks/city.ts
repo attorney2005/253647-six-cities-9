5cbd941d5b07ff8c09c7169a7f3286f034d9db5d
attorney2005 <72994919+attorney2005@users.noreply.github.com>
2022-03-13 20:21:20 +0300

    Рефакторинг

--- project/src/mocks/city.ts
+++ project/src/mocks/city.ts
@@ -3,8 +3,8 @@ import {City} from '../types/offer';
 export const CITY: City = {
   name: 'Amsterdam',
   location: {
-    latitude: 40.835292,
-    longitude: -73.916236,
+    latitude: 52.3909553943508,
+    longitude: 4.85309666406198,
     zoom: 10,
   },
 };
