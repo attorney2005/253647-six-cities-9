f5f06957485afdce2c0423fb0d214a842fda12f5
attorney2005 <72994919+attorney2005@users.noreply.github.com>
2022-03-14 21:16:49 +0300

    Рефакторинг

--- project/src/components/main-screen/main-screen.tsx
+++ project/src/components/main-screen/main-screen.tsx
@@ -3,6 +3,7 @@ import OfferCards from '../offer-cards/offer-cards';
 import {Offer, City, Offers} from '../../types/offer';
 import Map from '../map/map';
 import MainCityList from '../main-city-list/main-city-list';
+import MainSorting from '../main-sorting/main-sorting';
 
 type MainPageProps = {
   hotelsCount: number;
@@ -51,22 +52,8 @@ function MainScreen({hotelsCount, offers, city}: MainPageProps): JSX.Element {
             <div className="cities__places-container container">
               <section className="cities__places places">
                 <h2 className="visually-hidden">Places</h2>
-                <b className="places__found">{hotelsCount} places to stay in Amsterdam</b>
-                <form className="places__sorting" action="#" method="get">
-                  <span className="places__sorting-caption">Sort by</span>
-                  <span className="places__sorting-type" tabIndex={0}>
-                  Popular
-                    <svg className="places__sorting-arrow" width="7" height="4">
-                      <use xlinkHref="#icon-arrow-select"></use>
-                    </svg>
-                  </span>
-                  <ul className="places__options places__options--custom places__options--opened">
-                    <li className="places__option places__option--active" tabIndex={0}>Popular</li>
-                    <li className="places__option" tabIndex={0}>Price: low to high</li>
-                    <li className="places__option" tabIndex={0}>Price: high to low</li>
-                    <li className="places__option" tabIndex={0}>Top rated first</li>
-                  </ul>
-                </form>
+                <b className="places__found">{offers.length} places to stay in {city.name}</b>
+                <MainSorting/>
                 <OfferCards
                   offers={offers}
                   onListItemMouseEnter={onListItemMouseEnter}
