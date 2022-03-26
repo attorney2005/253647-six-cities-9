3710ac0644c84512961e7e98d16ae2133986b4cb
attorney2005 <72994919+attorney2005@users.noreply.github.com>
2022-03-26 21:18:03 +0300

    Сделал переход на ввод логина

--- project/src/components/header/header.tsx
+++ project/src/components/header/header.tsx
@@ -1,4 +1,5 @@
 import {Link} from 'react-router-dom';
+import {AppRoute} from '../../constant';
 
 function Header(): JSX.Element {
   return (
@@ -6,7 +7,7 @@ function Header(): JSX.Element {
       <div className="container">
         <div className="header__wrapper">
           <div className="header__left">
-            <Link className="header__logo-link" to='/'>
+            <Link className="header__logo-link" to={AppRoute.Main}>
               <img
                 className="header__logo"
                 src="img/logo.svg" alt="6 cities logo"
@@ -18,14 +19,14 @@ function Header(): JSX.Element {
           <nav className="header__nav">
             <ul className="header__nav-list">
               <li className="header__nav-item user">
-                <Link className="header__nav-link header__nav-link--profile" to="/">
+                <Link className="header__nav-link header__nav-link--profile" to={AppRoute.Favorites} >
                   <div className="header__avatar-wrapper user__avatar-wrapper">
                   </div>
                   <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
                 </Link>
               </li>
               <li className="header__nav-item">
-                <Link className="header__nav-link" to="/">
+                <Link className="header__nav-link" to={AppRoute.Sign_In}>
                   <span className="header__signout">Sign out</span>
                 </Link>
               </li>
