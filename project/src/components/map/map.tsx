c3277aa4024b70b7dad6ea8b67fd1ad4066ce581
attorney2005 <72994919+attorney2005@users.noreply.github.com>
2022-04-03 22:00:39 +0300

    Сделал оптимизацию рендеринга карт

--- project/src/components/map/map.tsx
+++ project/src/components/map/map.tsx
@@ -1,9 +1,6 @@
-import {useEffect, useRef} from 'react';
-import leaflet from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import useMap from '../../hooks/use-map';
 import {City, Offer, Offers} from '../../types/offer';
-import {URL_MARKER_DEFAULT, URL_MARKER_CURRENT} from '../../constant';
 
 type MapProps = {
   city: City;
@@ -12,45 +9,7 @@ type MapProps = {
 }
 
 function Map({city, offers, activeOffer}: MapProps) {
-  const mapRef = useRef(null);
-  const map = useMap(mapRef, city);
-
-  const defaultCustomIcon = leaflet.icon({
-    iconUrl: URL_MARKER_DEFAULT,
-    iconSize: [40, 40],
-    iconAnchor: [20, 40],
-  });
-
-  const currentCustomIcon = leaflet.icon({
-    iconUrl: URL_MARKER_CURRENT,
-    iconSize: [40, 40],
-    iconAnchor: [20, 40],
-  });
-  useEffect(() => {
-    if (!map) {
-      return;
-    }
-    const markers: leaflet.Marker[] = [];
-
-    offers.forEach((offer) => {
-      const marker = leaflet
-        .marker({
-          lat: offer.location.latitude,
-          lng: offer.location.longitude,
-        }, {
-          icon: (activeOffer && offer.location.latitude === activeOffer.location.latitude && offer.location.longitude === activeOffer.location.longitude)
-            ? currentCustomIcon
-            : defaultCustomIcon,
-        });
-      marker.addTo(map);
-      markers.push(marker);
-    });
-
-    return () => markers.forEach((marker) => {
-      marker.removeFrom(map);
-    });
-
-  }, [map, offers, activeOffer]);
+  const mapRef = useMap(city, offers, activeOffer);
 
   return (
     <div
--- project/src/hooks/use-map.tsx
+++ project/src/hooks/use-map.tsx
@@ -1,13 +1,27 @@
-import {MutableRefObject, useEffect, useState} from 'react';
+import {useEffect, useRef, useState} from 'react';
 import leaflet, {Map} from 'leaflet';
-import {City} from '../types/offer';
+import {City, Offer, Offers} from '../types/offer';
+import {URL_MARKER_CURRENT, URL_MARKER_DEFAULT} from '../constant';
 
-function useMap(mapRef: MutableRefObject<null>, city: City) {
+const defaultIcon = leaflet.icon({
+  iconUrl: URL_MARKER_DEFAULT,
+  iconSize: [40, 40],
+  iconAnchor: [20, 40],
+});
+
+const selectedOfferIcon = leaflet.icon({
+  iconUrl: URL_MARKER_CURRENT,
+  iconSize: [40, 40],
+  iconAnchor: [20, 40],
+});
+
+function useMap(city: City, offers: Offers, activeOffer: Offer | null) {
+  const ref = useRef(null);
   const [map, setMap] = useState<Map | null>(null);
 
   useEffect(() => {
-    if (mapRef.current !== null && map === null) {
-      const instance = leaflet.map(mapRef.current, {
+    if (!map && ref.current) {
+      const leafletMap = leaflet.map(ref.current, {
         center: {
           lat: city.location.latitude,
           lng: city.location.longitude,
@@ -15,29 +29,61 @@ function useMap(mapRef: MutableRefObject<null>, city: City) {
         zoom: city.location.zoom,
       });
 
-      leaflet
-        .tileLayer(
-          'https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png',
-          {
-            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-          },
-        )
-        .addTo(instance);
+      leaflet.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+      }).addTo(leafletMap);
 
-      setMap(instance);
+      setMap(leafletMap);
     }
-  }, [mapRef, map, city]);
+  }, [ref]);
 
   useEffect(() => {
-    if (map !== null) {
+    if (map) {
       map.flyTo({
         lat: city.location.latitude,
         lng: city.location.longitude,
       }, city.location.zoom, {duration: 0.9});
     }
-  }, [city, map]);
+  }, [city]);
 
-  return map;
+  useEffect(() => {
+    if (map) {
+      map.eachLayer((layer) => {
+        if (layer instanceof leaflet.Marker) {
+          layer.removeFrom(map);
+        }
+      });
+      offers.forEach((offer) => {
+        const marker = leaflet.marker({
+          lat: offer.location.latitude,
+          lng: offer.location.longitude,
+        }, {
+          icon: defaultIcon,
+        });
+        marker.addTo(map);
+      });
+    }
+  }, [offers, map]);
+
+  useEffect(() => {
+    if (map && activeOffer) {
+      const activeOfferLocation = leaflet.latLng([
+        activeOffer.location.latitude,
+        activeOffer.location.longitude,
+      ]);
+      map.eachLayer((layer) => {
+        if (layer instanceof leaflet.Marker) {
+          if (layer.getLatLng().equals(activeOfferLocation)) {
+            layer.setIcon(selectedOfferIcon);
+          } else {
+            layer.setIcon(defaultIcon);
+          }
+        }
+      });
+    }
+  }, [activeOffer]);
+
+  return ref;
 }
 
 export default useMap;
